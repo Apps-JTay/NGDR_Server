ac8fd84cba6ee090f91e46b960c03262539463b3
apps <apps@j-tay.com>
2019-07-16 13:35:19 -0400

    Created Email Address Collector form

--- NGDR_Server/Forms/EmailAddressCollector.Designer.cs
+++ NGDR_Server/Forms/EmailAddressCollector.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ultraLabel1 = new Infragistics.Win.Misc.UltraLabel();
+            this.ultraFormattedTextEditor1 = new Infragistics.Win.FormattedLinkLabel.UltraFormattedTextEditor();
+            this.btnSave = new Infragistics.Win.Misc.UltraButton();
+            this.btnDelete = new Infragistics.Win.Misc.UltraButton();
+            this.btnClose = new Infragistics.Win.Misc.UltraButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblAppVersion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblDB = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblFVersion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ultraLabel1
+            // 
+            this.ultraLabel1.Location = new System.Drawing.Point(13, 13);
+            this.ultraLabel1.Name = "ultraLabel1";
+            this.ultraLabel1.Size = new System.Drawing.Size(362, 23);
+            this.ultraLabel1.TabIndex = 0;
+            this.ultraLabel1.Text = "Enter a comma separated list of e-mail addresses to send the alerts";
+            // 
+            // ultraFormattedTextEditor1
+            // 
+            this.ultraFormattedTextEditor1.Location = new System.Drawing.Point(13, 46);
+            this.ultraFormattedTextEditor1.Name = "ultraFormattedTextEditor1";
+            this.ultraFormattedTextEditor1.Size = new System.Drawing.Size(664, 164);
+            this.ultraFormattedTextEditor1.TabIndex = 1;
+            this.ultraFormattedTextEditor1.Value = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(13, 241);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(95, 241);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(177, 241);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblAppVersion,
+            this.lblDB,
+            this.lblFVersion});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 303);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblAppVersion
+            // 
+            this.lblAppVersion.Name = "lblAppVersion";
+            this.lblAppVersion.Size = new System.Drawing.Size(81, 17);
+            this.lblAppVersion.Text = "lblAppVersion";
+            // 
+            // lblDB
+            // 
+            this.lblDB.Name = "lblDB";
+            this.lblDB.Size = new System.Drawing.Size(35, 17);
+            this.lblDB.Text = "lblDB";
+            // 
+            // lblFVersion
+            // 
+            this.lblFVersion.Name = "lblFVersion";
+            this.lblFVersion.Size = new System.Drawing.Size(65, 17);
+            this.lblFVersion.Text = "lblFVersion";
+            // 
+            // EmailAddressCollector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "EmailAddressCollector";
+            this.ClientSize = new System.Drawing.Size(800, 325);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.ultraFormattedTextEditor1);
+            this.Controls.Add(this.ultraLabel1);
+            this.Name = "EmailAddressCollector";
+            this.Text = "Email Address Collector";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Infragistics.Win.Misc.UltraLabel ultraLabel1;
+        private Infragistics.Win.FormattedLinkLabel.UltraFormattedTextEditor ultraFormattedTextEditor1;
+        private Infragistics.Win.Misc.UltraButton btnSave;
+        private Infragistics.Win.Misc.UltraButton btnDelete;
+        private Infragistics.Win.Misc.UltraButton btnClose;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblAppVersion;
+        private System.Windows.Forms.ToolStripStatusLabel lblDB;
+        private System.Windows.Forms.ToolStripStatusLabel lblFVersion;
     }
 }
