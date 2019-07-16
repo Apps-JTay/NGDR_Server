7d1ad9429a4336a83653ec1e8085dc411e55bec3
apps <apps@j-tay.com>
2019-07-16 13:09:08 -0400

    Recreating NGDR_nightly file transfer

---
+++ NGDR_Server/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NGDR_Server
+{
+    public partial class DR_Server : Form
+    {
+        public DR_Server()
+        {
+            InitializeComponent();
+        }
+    }
+}
