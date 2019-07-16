1e0fccc1cc2ab8e729dc8a8efc8e5e1e9eaedc8c
apps <apps@j-tay.com>
2019-07-16 15:58:19 -0400

    Created Connection String Manager form

---
+++ NGDR_Server/Forms/ConnectionString.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace NGDR_Server.Forms
+{
+    partial class ConnectionString
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectionStringManager1 = new JTay.ConnectionStringManager.ConnectionStringManager();
+            this.SuspendLayout();
+            // 
+            // connectionStringManager1
+            // 
+            this.connectionStringManager1.Location = new System.Drawing.Point(2, 2);
+            this.connectionStringManager1.Name = "connectionStringManager1";
+            this.connectionStringManager1.Size = new System.Drawing.Size(1057, 339);
+            this.connectionStringManager1.TabIndex = 0;
+            // 
+            // ConnectionString
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 353);
+            this.Controls.Add(this.connectionStringManager1);
+            this.Name = "ConnectionString";
+            this.Text = "Connection String";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private JTay.ConnectionStringManager.ConnectionStringManager connectionStringManager1;
+    }
+}
--- NGDR_Server/Forms/DR_Server.cs
+++ NGDR_Server/Forms/DR_Server.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using JTay.FrameWork;
+using JTay.FrameWork.Enumerators;
+using JTay.FrameWork.Interfaces;
+using JTay.FrameWork.Support;
 
 namespace NGDR_Server
 {
     public partial class DR_Server : Form
     {
+        private readonly bool fromCMD;
+        private readonly string location;
+        private readonly bool sameProvider;
+        private readonly bool limitEnc;
+        private readonly bool unZip;
+        private readonly int limitNum;
+        private  static readonly  IDataLayer ssDataLayer = DataLayerFactory.CreateDataLayer(DataSourceType.MsSql, ConnectionStrings.ConnectionString("FT"));
+
+        private readonly ArrayList locationList = new ArrayList();
+
         public DR_Server()
         {
             InitializeComponent();
